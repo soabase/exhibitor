12a73b509cdc071724d5732db4200e489e7856b6
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-02-13 16:56:20 -0800

    Fixed tests for new spec format

--- exhibitor-core/src/test/java/com/netflix/exhibitor/core/state/TestServerList.java
+++ exhibitor-core/src/test/java/com/netflix/exhibitor/core/state/TestServerList.java
@@ -9,26 +9,26 @@ public class TestServerList
     @Test
     public void     testSimple()
     {
-        ServerList      serverList = new ServerList("a:1");
+        ServerList      serverList = new ServerList("1:a");
         Assert.assertEquals(serverList.getSpecs(), Arrays.asList(new ServerList.ServerSpec("a", 1)));
 
-        serverList = new ServerList("a:1,b:2");
+        serverList = new ServerList("1:a,2:b");
         Assert.assertEquals(serverList.getSpecs(), Arrays.asList(new ServerList.ServerSpec("a", 1), new ServerList.ServerSpec("b", 2)));
 
-        serverList = new ServerList("b:2,a:1");
+        serverList = new ServerList("2:b,1:a");
         Assert.assertEquals(serverList.getSpecs(), Arrays.asList(new ServerList.ServerSpec("b", 2), new ServerList.ServerSpec("a", 1)));
 
-        serverList = new ServerList("b:2,a:1");
+        serverList = new ServerList("2:b,1:a");
         Assert.assertNotEquals(serverList.getSpecs(), Arrays.asList(new ServerList.ServerSpec("a", 1), new ServerList.ServerSpec("b", 2)));
     }
 
     @Test
     public void     testEdges()
     {
-        ServerList      serverList = new ServerList("a:1,,,a,asfklasf,L:1x30,:*&*,$:$");
+        ServerList      serverList = new ServerList("1:a,,,a,asfklasf,L:1x30,:*&*,$:$");
         Assert.assertEquals(serverList.getSpecs(), Arrays.asList(new ServerList.ServerSpec("a", 1)));
 
-        serverList = new ServerList("a :1    , b :    2 \t\t,     , : ,");
+        serverList = new ServerList("1 :a    , 2 :    b \t\t,     , : ,");
         Assert.assertEquals(serverList.getSpecs(), Arrays.asList(new ServerList.ServerSpec("a", 1), new ServerList.ServerSpec("b", 2)));
     }
 }
