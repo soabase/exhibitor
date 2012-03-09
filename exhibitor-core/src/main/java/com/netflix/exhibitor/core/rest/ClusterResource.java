5842a0aafa8595d23327f6650a52810af8af7eee
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-03-09 10:00:46 -0800

    Added copyright

--- exhibitor-core/src/main/java/com/netflix/exhibitor/core/rest/ClusterResource.java
+++ exhibitor-core/src/main/java/com/netflix/exhibitor/core/rest/ClusterResource.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2011 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
 package com.netflix.exhibitor.core.rest;
 
 import com.google.common.cache.CacheBuilder;
@@ -142,19 +160,19 @@ public class ClusterResource
     public String remoteGetLog(@Context UriInfo uriInfo, @PathParam("hostname") String hostname) throws Exception
     {
         return makeRemoteRequest
-        (
-            uriInfo,
-            hostname,
-            false,
-            new Callable<String>()
-            {
-                @Override
-                public String call() throws Exception
+            (
+                uriInfo,
+                hostname,
+                false,
+                new Callable<String>()
                 {
-                    return getLog();
+                    @Override
+                    public String call() throws Exception
+                    {
+                        return getLog();
+                    }
                 }
-            }
-        );
+            );
     }
 
     @Path("log")
