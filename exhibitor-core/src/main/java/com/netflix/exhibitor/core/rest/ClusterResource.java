4ba8b40009b6d6240aa4870b9933537efa9efc41
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-07-06 22:24:02 -0700

    update Apache header

--- exhibitor-core/src/main/java/com/netflix/exhibitor/core/rest/ClusterResource.java
+++ exhibitor-core/src/main/java/com/netflix/exhibitor/core/rest/ClusterResource.java
@@ -1,19 +1,17 @@
 /*
+ * Copyright 2012 Netflix, Inc.
  *
- *  Copyright 2011 Netflix, Inc.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.netflix.exhibitor.core.rest;
@@ -45,9 +43,6 @@ import javax.ws.rs.ext.ContextResolver;
 import java.net.URLEncoder;
 import java.util.concurrent.Callable;
 
-/**
- * REST calls for dealing with the entire ensemble
- */
 @SuppressWarnings("UnusedDeclaration")
 @Path("exhibitor/v1/cluster")
 public class ClusterResource
