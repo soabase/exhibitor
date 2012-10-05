e10585f80c0188a9886681eeef3dc2204821e720
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-10-05 16:00:39 -0700

    removed some unused code

--- exhibitor-core/src/test/java/com/netflix/exhibitor/core/analyze/TestPathAnalyzer.java
+++ exhibitor-core/src/test/java/com/netflix/exhibitor/core/analyze/TestPathAnalyzer.java
@@ -22,7 +22,6 @@ import com.netflix.exhibitor.core.Exhibitor;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -37,11 +36,6 @@ public class TestPathAnalyzer
         data.put(new PathAndMax("/r3", 1), Arrays.asList("p2-0001", "p5-0002"));
         data.put(new PathAndMax("/r4", 1), Arrays.asList("p2-0001", "p3-0002"));
         data.put(new PathAndMax("/r5", 1), Arrays.asList("p3-0001", "p4-0002"));
-/*
-        data.put(new PathAndMax("/r1", 1), Arrays.asList("p1-0001", "p2-0002", "p3-0003"));
-        data.put(new PathAndMax("/r2", 1), Arrays.asList("p2-0001", "p1-0002", "p3-0003"));
-*/
-
 
         Exhibitor       mockExhibitor = Mockito.mock(Exhibitor.class);
         PathAnalyzer    pathAnalyzer = new PathAnalyzer(mockExhibitor, Lists.newArrayList(data.keySet()))
@@ -67,31 +61,5 @@ public class TestPathAnalyzer
         System.out.println();
         System.out.println("Cycles:");
         System.out.println(analysis.getPossibleCycles());
-    }
-
-    private static final String         indentStr = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
-
-    private void outputNode(Node n, int flagValue, int indent)
-    {
-        System.out.print(indentStr.substring(0, indent));
-        if ( n.getFlagValue() == flagValue )
-        {
-            System.out.println("CYCLE:" + toStr(n));
-        }
-        else
-        {
-            n.setFlagValue(flagValue);
-            System.out.println(toStr(n));
-
-            for ( Node child : n.getEdges() )
-            {
-                outputNode(child, flagValue, indent + 1);
-            }
-        }
-    }
-
-    private String toStr(Node n)
-    {
-        return n.getValue() + " ";
     }
 }
